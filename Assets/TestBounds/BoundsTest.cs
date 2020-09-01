c63ad5d699e4ae70c342ee6cf744fce9aa5d0052
unknown <zhanpeng.xie@gtadata.com>
2020-09-01 15:21:15 +0800

    画线涂鸦工具初始化

---
+++ Assets/TestBounds/BoundsTest.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[ExecuteInEditMode]
+public class BoundsTest : MonoBehaviour
+{
+    Bounds bd;
+    SpriteRenderer spr;
+    // Start is called before the first frame update
+    void Start()
+    {
+        bd = GetComponent<MeshRenderer>().bounds;//bounds就是模型包围盒的大小
+        //spr = GetComponent<SpriteRenderer>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Start();
+        Debug.Log(bd.size+"  update");
+        //Debug.Log(spr.bounds.size+ "  update "+ " size= "+spr.sprite.rect.width);
+
+    }
+}
